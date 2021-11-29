500c9da0aba14fbd9e393a6070c825110ea9d5a8
fivetran-joemarkiewicz <joseph.markiewicz@fivetran.com>
2021-11-29 14:31:04 -0600

    most recent record added to insights

--- models/stg_instagram_business__media_insights.sql
+++ models/stg_instagram_business__media_insights.sql
@@ -50,6 +50,15 @@ final as (
         video_views,
         source_relation
     from fields
+),
+
+is_most_recent as (
+
+    select 
+        *,
+        row_number() over (partition by post_id, source_relation order by _fivetran_synced desc) = 1 as is_most_recent_record
+    from final
+
 )
 
 select * from final
